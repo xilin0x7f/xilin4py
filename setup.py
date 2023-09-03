4060bf39b08c9d7f589c333d0666944c9abaf55d
xilin0x7f <hkychn@outlook.com>
2023-09-03 23:30:16 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+# Author: 赩林, xilin0x7f@163.com
+from setuptools import setup
+
+setup(
+    name='xilin4py',
+    version='0.1',
+    description='A sample Python package',
+    author='xilin0x7f',
+    author_email='xilin0x7f@163.com',
+    packages=['xilin4py'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'sklearn'
+    ],
+)
