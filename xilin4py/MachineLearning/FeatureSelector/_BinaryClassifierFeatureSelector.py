84b94b79aac433aa3f6533c6cb7af5d377583a51
xilin0x7f <xilin0x7f@outlook.com>
2023-09-28 14:39:47 +0800

    add get_support

--- xilin4py/MachineLearning/FeatureSelector/_BinaryClassifierFeatureSelector.py
+++ xilin4py/MachineLearning/FeatureSelector/_BinaryClassifierFeatureSelector.py
@@ -5,6 +5,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.stats import ttest_ind, chi2_contingency
 from sklearn.linear_model import LassoCV, LogisticRegressionCV
 from xilin4py.Radiomics import icc_compute_optimized
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 def f_score_old(x, y):
     scores = []
@@ -78,8 +79,12 @@ class RecursivePCorrFeatureSelector(BaseEstimator, TransformerMixin):
     def __init__(self, threshold_p_value=0.05, threshold_correlation=0.9):
         self.threshold_p_value = threshold_p_value
         self.threshold_correlation = threshold_correlation
+        self.selected_columns_ = None
+        self.support_mask_ = None
+        self.n_features_in_ = None
 
     def fit(self, x, y=None):
+        self.n_features_in_ = x.shape[1]
         p_values = compute_p_values(x, y)
         index_mapping = dict(zip(range(x.shape[1]), range(x.shape[1])))
         deleted_columns = np.argwhere(p_values >= self.threshold_p_value)
@@ -104,10 +109,20 @@ class RecursivePCorrFeatureSelector(BaseEstimator, TransformerMixin):
             x_selected = np.delete(x_selected, deleted_columns, axis=1)
 
         self.selected_columns_ = list(index_mapping.values())
+        self.support_mask_ = self._get_support_mask()
         return self
 
     def transform(self, x, y=None):
         return x[:, self.selected_columns_]
+
+    def get_support(self):
+        check_is_fitted(self)
+        return self.support_mask_
+
+    def _get_support_mask(self):
+        support_mask = np.zeros(self.n_features_in_, dtype=bool)
+        support_mask[self.selected_columns_] = True
+        return support_mask
 
 
 class LassoFeatureSelector(BaseEstimator, TransformerMixin):
@@ -115,22 +130,41 @@ class LassoFeatureSelector(BaseEstimator, TransformerMixin):
         self.cv = cv
         self.Cs = Cs  # Cs describes the inverse of regularization strength
         self.random_state = random_state
+        self.log_reg = None
+        self.selected_columns_ = None
+        self.support_mask_ = None
+        self.n_features_in_ = None
 
     def fit(self, x, y):
+        self.n_features_in_ = x.shape[1]
         self.log_reg = LogisticRegressionCV(
             cv=self.cv, penalty='l1', solver='liblinear', Cs=self.Cs, random_state=self.random_state)
         self.log_reg.fit(x, y)
-        self.selected_features_ = np.nonzero(self.log_reg.coef_[0])[0]
+        self.selected_columns_ = np.nonzero(self.log_reg.coef_[0])[0]
+        self.support_mask_ = self._get_support_mask()
         return self
 
     def transform(self, x, y=None):
-        return x[:, self.selected_features_]
+        return x[:, self.selected_columns_]
+
+    def get_support(self):
+        check_is_fitted(self)
+        return self.support_mask_
+
+    def _get_support_mask(self):
+        support_mask = np.zeros(self.n_features_in_, dtype=bool)
+        support_mask[self.selected_columns_] = True
+        return support_mask
 
 class FeatureSelectorICC(BaseEstimator, TransformerMixin):
     def __init__(self, threshold=0.75):
         self.threshold = threshold
+        self.n_features_in_ = 0
+        self.support_mask_ = None
+        self.selected_columns_ = None
 
     def fit(self, x, y=None):
+        self.n_features_in_ = x.shape[1]
         # Split the data into two halves
         mid_col = x.shape[1] // 2
         data1 = x[:, :mid_col]
@@ -142,12 +176,22 @@ class FeatureSelectorICC(BaseEstimator, TransformerMixin):
 
         icc = icc_compute_optimized(data1=df1, data2=df2)
         self.selected_columns_ = icc[icc["icc"] > self.threshold].index
+        self.support_mask_ = self._get_support_mask()
         return self
 
     def transform(self, x, y=None):
         mid_col = x.shape[1] // 2
         data1 = x[:, :mid_col]
         return data1[:, self.selected_columns_]
+
+    def get_support(self):
+        check_is_fitted(self)
+        return self.support_mask_
+
+    def _get_support_mask(self):
+        support_mask = np.zeros(self.n_features_in_, dtype=bool)
+        support_mask[self.selected_columns_] = True
+        return support_mask
 
 
 if __name__ == "__main__":
