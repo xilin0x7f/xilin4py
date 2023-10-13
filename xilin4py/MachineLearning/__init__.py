329b223e96d88205508071bf2d1ebaa4c76dbf11
xilin0x7f <hkychn@outlook.com>
2023-10-13 10:18:07 +0800

    add get strong feature

--- xilin4py/MachineLearning/__init__.py
+++ xilin4py/MachineLearning/__init__.py
@@ -1,6 +1,7 @@
 from .test import test_fun
 from ._CrossValidate import CrossValidationEvaluator, NestedCrossValidationEvaluator
 from ._Split import CustomSplit
+from ._get_strong_features import get_strong_feature
 
 __all__ = [
     "CrossValidationEvaluator",
---
+++ xilin4py/MachineLearning/_get_strong_features.py
@@ -0,0 +1,43 @@
+# Author: 赩林, xilin0x7f@163.com
+import os
+import copy
+import numpy as np
+import pandas as pd
+
+def get_strong_feature(cv_evaluator, columns, x, y, save_dir, model_name):
+    # get strong feature and data, then save
+    feature_selection_counts = np.zeros(len(columns), dtype=int)
+    feature_coef_sum = np.zeros(len(columns))
+    for fitted_pipeline in cv_evaluator.pipeline_out_fitted:
+        selected_steps = [step for name, step in fitted_pipeline.named_steps.items() if name.endswith('_selector')]
+        feature_mask = None
+        for idx, step in enumerate(selected_steps):
+            if idx == 0:
+                feature_mask = copy.deepcopy(step.get_support())
+            else:
+                feature_mask[feature_mask] = feature_mask[feature_mask] & step.get_support()
+
+        feature_selection_counts += feature_mask.astype(int)
+
+        if hasattr(fitted_pipeline[-1], 'coef_'):
+            full_coef = np.zeros(len(feature_mask))
+            full_coef[feature_mask] = fitted_pipeline[-1].coef_.flatten()
+            feature_coef_sum += full_coef
+
+        if hasattr(fitted_pipeline[-1], 'feature_importances_'):
+            full_coef = np.zeros(len(feature_mask))
+            full_coef[feature_mask] = fitted_pipeline[-1].feature_importances_.flatten()
+            feature_coef_sum += full_coef
+
+    feature_selection_freq = feature_selection_counts / len(cv_evaluator.pipeline_out_fitted)
+    feature_coef_mean = feature_coef_sum / len(cv_evaluator.pipeline_out_fitted)
+    feature_selection_df = pd.DataFrame({
+        "feature": columns[feature_selection_freq > 0],
+        "freq": feature_selection_freq[feature_selection_freq > 0],
+        "mean_coef": feature_coef_mean[feature_selection_freq > 0]
+    })
+    feature_selection_df.to_excel(os.path.join(save_dir, f"FeatureSelected_{model_name}.xlsx"), index=False)
+    data_strong_feature = x[:, feature_selection_freq == 1]
+    data_strong_feature = pd.DataFrame(data_strong_feature, columns=columns[feature_selection_freq == 1])
+    data_strong_feature["label"] = y
+    data_strong_feature.to_excel(os.path.join(save_dir, f"DataStrongFeature_{model_name}.xlsx"), index=False)
