ef18fa7bce091c020b7dbf6e5e21f361f5dbcba8
xilin0x7f <hkychn@outlook.com>
2023-10-15 14:42:04 +0800

    delete not used code

--- xilin4py/MachineLearning/FeatureSelector/_BinaryClassifierFeatureSelector.py
+++ xilin4py/MachineLearning/FeatureSelector/_BinaryClassifierFeatureSelector.py
@@ -8,22 +8,6 @@ from xilin4py.Radiomics import icc_compute_optimized
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 import copy
 
-def f_score_old(x, y):
-    scores = []
-    # F score
-    for col in x.T:
-        data = np.column_stack([col, y])
-        data_pos = data[data[:, 1] == 1]
-        data_neg = data[data[:, 1] == 0]
-        score = (np.power(np.mean(data_pos[:, 0]) - np.mean(data[:, 0]), 2) +
-                 np.power(np.mean(data_neg[:, 0]) - np.mean(data[:, 0]), 2)) / (
-                        np.sum(np.power(data_pos[:, 0] - np.mean(data_pos[:, 0]), 2)) / (len(data_pos) - 1) +
-                        np.sum(np.power(data_neg[:, 0] - np.mean(data_neg[:, 0]), 2)) / (len(data_neg) - 1)
-                )
-        scores.append(score)
-
-    return scores, 1/np.array(scores)
-
 def f_score(x, y):
     # Calculate means once to avoid repeated calculations
     mean_data = np.mean(x, axis=0)
