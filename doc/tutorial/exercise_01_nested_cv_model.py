b9210316a212e679b22600188ff735366b101c58
xilin0x7f <hkychn@outlook.com>
2024-01-05 21:28:40 +0800

    240105

--- doc/tutorial/exercise_01_nested_cv_model.py
+++ doc/tutorial/exercise_01_nested_cv_model.py
@@ -3,10 +3,14 @@ import copy
 import os
 import random
 import warnings
+import itertools
+import imblearn
+import sys
 
+sys.path.insert(0, "C:/AppsData/PyCharm/xilin4py/xilin4py")
 import numpy as np
 import pandas as pd
-from sklearnex import patch_sklearn
+# from sklearnex import patch_sklearn
 
 # patch_sklearn()
 from sklearn import pipeline, preprocessing, metrics, feature_selection, model_selection, base
@@ -16,29 +20,30 @@ from sklearn.linear_model import LogisticRegression
 from joblib import Parallel, delayed
 from xilin4py.MachineLearning import NestedCrossValidationEvaluator
 from xilin4py.MachineLearning.FeatureSelector import f_score
+from imblearn.over_sampling import SMOTE
 warnings.filterwarnings("ignore")
 
 # permutation function
 def worker(task):
-    results_dir, idx, model_name = task
-    pipeline_out_new = base.clone(pipeline_out)
-    y_new = copy.deepcopy(y)
-    random.shuffle(y)
-    pipeline_out_new.fit(x, y_new)
+    results_dir, idx, cv_evaluator, model_name = task
+    cv_evaluator = copy.deepcopy(cv_evaluator)
+    random.shuffle(cv_evaluator.y)
+    cv_evaluator.run()
     print(model_name, idx)
-    print(f"ACC: {metrics.accuracy_score(y, pipeline_out_new.predict(x))}")
+    print(f"ACC: {metrics.accuracy_score(y, cv_evaluator.y_pred)}")
     np.savetxt(os.path.join(results_dir, f"pred_{model_name}_{idx:05}.txt"),
-               np.column_stack([y_new, pipeline_out_new.predict(x), pipeline_out_new.predict_proba(x)]))
+               np.column_stack([cv_evaluator.y, cv_evaluator.y_pred, cv_evaluator.y_prob]))
 
 
-results_dir = r"./data"
+results_dir = r"./doc/tutorial/data"
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
 
-transform_by_out, transform_range = True, [0, 1]
+transform_by_out, transform_range = True, [0, 1, 2]
 pipeline_out_pre_steps = [
     ("scaler", preprocessing.StandardScaler()),
-    ("fscore_selector", feature_selection.SelectKBest(f_score, k=10))
+    ("fscore_selector", feature_selection.SelectKBest(f_score, k=10)),
+    ("smote", SMOTE(random_state=1))
 ]
 
 ml_params = [
@@ -50,35 +55,44 @@ ml_params = [
      {"model__n_estimators": [10 * i for i in range(5, 21)]})
 ]
 
-
-
-cv_out = model_selection.StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
-cv_in = model_selection.StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+cv_out = model_selection.StratifiedKFold(n_splits=3, shuffle=True, random_state=0)
+cv_in = model_selection.StratifiedKFold(n_splits=2, shuffle=True, random_state=0)
 
 n_peru = 10
 n_peru_start = 0
 
-for ml_task in ml_tasks:
-
-
+data_paths = ["./doc/tutorial/data/binary_data.csv"]
+combinations = itertools.product(ml_params, data_paths)
+for combination in combinations:
+    ml_param, data_path = combination
+    data = pd.read_csv(data_path)
+    x = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
     columns = x.columns
     x, y = np.array(x), np.array(y)
 
-    ml_param = (ml_task, SVC(random_state=0, probability=True, kernel="linear"),
-                {"pca__n_components": np.linspace(0.1, 0.9, 9)})
-
     model_name, model, search_params = ml_param
+    if len(data_paths) > 1:
+        model_name = model_name + str(data_paths.index(data_path))
+
     print(model_name)
     pipeline_out_post_steps = [
         ("model", model)
     ]
-    pipeline_out = base.clone(pipeline.Pipeline(pipeline_out_pre_steps + pipeline_out_post_steps))
+    # pipeline_out = base.clone(pipeline.Pipeline(pipeline_out_pre_steps + pipeline_out_post_steps))
+    # if transform_by_out:
+    #     new_steps = [step for i, step in enumerate(pipeline_out.steps) if i not in transform_range]
+    #     pipeline_in = base.clone(pipeline.Pipeline(new_steps))
+    # else:
+    #     pipeline_in = base.clone(pipeline_out)
+
+    pipeline_out = imblearn.pipeline.Pipeline(pipeline_out_pre_steps + pipeline_out_post_steps)
 
     if transform_by_out:
         new_steps = [step for i, step in enumerate(pipeline_out.steps) if i not in transform_range]
-        pipeline_in = base.clone(pipeline.Pipeline(new_steps))
+        pipeline_in = imblearn.pipeline.clone(imblearn.pipeline.Pipeline(new_steps))
     else:
-        pipeline_in = base.clone(pipeline_out)
+        pipeline_in = imblearn.pipeline.clone(pipeline_out)
 
     cv_evaluator = NestedCrossValidationEvaluator(x, y, pipeline_out, pipeline_in, cv_out, cv_in, transform_by_out,
                                                   transform_range, search_params, verbose=False)
