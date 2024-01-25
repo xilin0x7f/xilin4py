9556f384a06e9a0ebc61e9482e712d0bde7432b8
xilin0x7f <hkychn@outlook.com>
2024-01-25 13:26:36 +0800

    add feature selection to init

--- xilin4py/MachineLearning/FeatureSelector/__init__.py
+++ xilin4py/MachineLearning/FeatureSelector/__init__.py
@@ -1,5 +1,5 @@
 # Author: 赩林, xilin0x7f@163.com
 from ._RegressionFeatureSelector import corr_score
 from ._BinaryClassifierFeatureSelector import (f_score, FeatureSelectorICC, LassoFeatureSelector,
-                                               RecursivePCorrFeatureSelector)
+                                               RecursivePCorrFeatureSelector, TtestFeatureSelector)
 from ._ReliefF import ExtendedReliefF
