a68bd2b7e0fc45733a9638b2b9a9e5b47e03dd55
xilin0x7f <hkychn@outlook.com>
2024-02-15 09:21:38 +0800

    more robust

--- xilin4py/MachineLearning/_get_strong_features.py
+++ xilin4py/MachineLearning/_get_strong_features.py
@@ -9,12 +9,17 @@ def get_strong_feature(cv_evaluator, columns, x, y, save_dir, model_name, endswi
     if not isinstance(columns, np.ndarray):
         columns = np.array(columns)
     # 在使用单层的交叉验证时，使用的是pipeline_fitted, 因此进行copy到pipeline_out_fitted，使其和嵌套交叉验证一致
-    if not hasattr(cv_evaluator, "pipeline_out_fitted"):
-        setattr(cv_evaluator, "pipeline_out_fitted", cv_evaluator.pipeline_fitted)
+    # if not hasattr(cv_evaluator, "pipeline_out_fitted"):
+    #     setattr(cv_evaluator, "pipeline_out_fitted", cv_evaluator.pipeline_fitted)
 
     feature_selection_counts = np.zeros(len(columns), dtype=int)
     feature_coef_sum = np.zeros(len(columns))
-    for fitted_pipeline in cv_evaluator.pipeline_out_fitted:
+    if hasattr(cv_evaluator, "pipeline_out_fitted"):
+        dest_pipelines = cv_evaluator.pipeline_out_fitted
+    else:
+        dest_pipelines = cv_evaluator.pipeline_fitted
+
+    for fitted_pipeline in dest_pipelines:
         selected_steps = [step for name, step in fitted_pipeline.named_steps.items() if name.endswith(endswith)]
         feature_mask = None
         for idx, step in enumerate(selected_steps):
@@ -35,8 +40,8 @@ def get_strong_feature(cv_evaluator, columns, x, y, save_dir, model_name, endswi
             full_coef[feature_mask] = fitted_pipeline[-1].feature_importances_.flatten()
             feature_coef_sum += full_coef
 
-    feature_selection_freq = feature_selection_counts / len(cv_evaluator.pipeline_out_fitted)
-    feature_coef_mean = feature_coef_sum / len(cv_evaluator.pipeline_out_fitted)
+    feature_selection_freq = feature_selection_counts / len(dest_pipelines)
+    feature_coef_mean = feature_coef_sum / len(dest_pipelines)
     feature_selection_df = pd.DataFrame({
         "feature": columns[feature_selection_freq > 0],
         "freq": feature_selection_freq[feature_selection_freq > 0],
