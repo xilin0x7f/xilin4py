0e4996b89c5bf47098318fe6c1989b7367a53a7f
xilin0x7f <xilin0x7f@outlook.com>
2024-07-27 16:24:58 +0800

    add surface function

--- xilin4py/Surfaces/surfaces_fn.py
+++ xilin4py/Surfaces/surfaces_fn.py
@@ -1,10 +1,84 @@
 # Author: 赩林, xilin0x7f@163.com
 import numpy as np
+import nibabel as nib
 
 def compute_surface_area(vertices, faces):
     vertex_areas = np.zeros(vertices.shape[0])
     for face in faces:
         v0, v1, v2 = vertices[face[0]], vertices[face[1]], vertices[face[2]]
-        vertex_areas[face] = np.linalg.norm(np.cross(v1 - v0, v2 - v0)) / 6.0
+        vertex_areas[face] += np.linalg.norm(np.cross(v1 - v0, v2 - v0)) / 6.0
 
     return vertex_areas
+
+def cifti_separate(cifti_file):
+    cifti_data = cifti_file.get_fdata()
+    brain_models = cifti_file.header.matrix.get_index_map(1).brain_models
+
+    volume_data, volume_mat = None, None
+    ax1 = cifti_file.header.get_axis(1)
+    if ax1._volume_shape is not None:
+        volume_dim = ax1._volume_shape
+        volume_mat = ax1._affine
+        volume_data = np.zeros(list(volume_dim) + [cifti_data.shape[0]])
+
+    cortex_left_data, cortex_left_indices = None, None
+    cortex_right_data, cortex_right_indices = None, None
+
+    for bm in brain_models:
+        if bm.brain_structure == "CIFTI_STRUCTURE_CORTEX_LEFT":
+            cortex_left_data = np.zeros([cifti_data.shape[0], bm.surface_number_of_vertices])
+            cortex_left_indices = bm._vertex_indices._indices
+            cortex_left_data[:, cortex_left_indices] = cifti_data[:, bm.index_offset:bm.index_offset + bm.index_count]
+
+        elif bm.brain_structure == "CIFTI_STRUCTURE_CORTEX_RIGHT":
+            cortex_right_data = np.zeros([cifti_data.shape[0], bm.surface_number_of_vertices])
+            cortex_right_indices = bm._vertex_indices._indices
+            cortex_right_data[:, cortex_right_indices] = cifti_data[:, bm.index_offset:bm.index_offset + bm.index_count]
+
+        elif bm.model_type == "CIFTI_MODEL_TYPE_VOXELS":
+            indices = np.array(bm._voxel_indices_ijk._indices)
+            volume_data[indices[:, 0], indices[:, 1], indices[:, 2], :] = cifti_data[:, bm.index_offset:bm.index_offset + bm.index_count].T
+
+    return cortex_left_data, cortex_right_data, cortex_left_indices, cortex_right_indices, volume_data, volume_mat
+
+def cifti_surface_zscore(cifti_file, mask_left=None, mask_right=None):
+    cortex_left_data, cortex_right_data, cortex_left_indices, cortex_right_indices = cifti_separate(cifti_file)[:4]
+    if mask_left is None:
+        mask_left = np.zeros(cortex_left_data.shape[1])
+        mask_left[cortex_left_indices] = 1
+
+    if mask_right is None:
+        mask_right = np.zeros(cortex_right_data.shape[1])
+        mask_right[cortex_right_indices] = 1
+
+    mask_left, mask_right = mask_left.flatten(), mask_right.flatten()
+    cortex_all = np.hstack([cortex_left_data, cortex_right_data])
+    mask_all = np.hstack([mask_left, mask_right])
+    cortex_masked = cortex_all[:, mask_all > 0]
+    cortex_masked_zscore = (cortex_masked - np.nanmean(cortex_masked, axis=1)[:, np.newaxis]) / np.nanstd(cortex_masked, axis=1)[:, np.newaxis]
+
+    # cortex_all_zscore = cortex_all * 0
+    # cortex_all_zscore[:, mask_all > 0] = cortex_masked_zscore
+
+    mask_left_vertex = np.array(range(cortex_left_data.shape[1]))
+    mask_left_vertex = mask_left_vertex[mask_left > 0]
+
+    mask_right_vertex = np.array(range(cortex_right_data.shape[1]))
+    mask_right_vertex = mask_right_vertex[mask_right > 0]
+
+    # ax0 = cifti_file.header.get_axis(0)
+    ax0 = nib.cifti2.cifti2_axes.ScalarAxis(name=[f"#{i+1}" for i in range(cortex_all.shape[0])])
+    # ax1 = cifti_file.header.get_axis(1)
+    ax1 = nib.cifti2.cifti2_axes.BrainModelAxis(
+        name=np.array(['CIFTI_STRUCTURE_CORTEX_LEFT' for i in range(np.sum(mask_left > 0))] +
+                      ['CIFTI_STRUCTURE_CORTEX_RIGHT' for i in range(np.sum(mask_right > 0))]),
+        nvertices={
+            "CIFTI_STRUCTURE_CORTEX_LEFT": cortex_left_data.shape[1],
+            "CIFTI_STRUCTURE_CORTEX_RIGHT": cortex_right_data.shape[1]},
+        vertex=np.hstack([mask_left_vertex, mask_right_vertex])
+    )
+    header = nib.Cifti2Header.from_axes((ax0, ax1))
+    img = nib.Cifti2Image(cortex_masked_zscore, header)
+    img.nifti_header.set_intent('ConnDenseScalar')
+
+    return img
