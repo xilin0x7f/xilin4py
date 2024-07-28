fcd0f0ae6a588cb33c3dc77464862e6bcc49e8f0
xilin0x7f <hkychn@outlook.com>
2024-07-28 08:41:10 +0800

    add fn to surfaces

--- xilin4py/Surfaces/__init__.py
+++ xilin4py/Surfaces/__init__.py
@@ -1,4 +1,4 @@
 # Author: 赩林, xilin0x7f@163.com
-from .surfaces_fn import compute_surface_area
+from .surfaces_fn import compute_surface_area, cifti_surface_zscore, cifti_separate
 
-__all__ = ['compute_surface_area']
+__all__ = ['compute_surface_area', 'cifti_surface_zscore', 'cifti_separate']
